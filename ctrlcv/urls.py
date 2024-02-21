9dec22cb4fb2ab6b7712fd83d399f9d369e76728
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-02-21 16:52:30 +0530

    add ctrlcv main folder

---
+++ ctrlcv/urls.py
@@ -0,0 +1,44 @@
+"""ctrlcv URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from ctrlcv import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from home.views import about
+urlpatterns = [
+    path("", include('home.urls')),
+    path('product/' , include('products.urls')),
+    path('accounts/' , include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    # path('', views.homePage),
+    # path('login/', views.login),
+    # path('register/', views.register),
+    # path('about/', include('home.urls')),
+    # path('service/', views.service),
+    # path('secure-us/', views.secureUs),
+    # path('contact/', views.contact),
+    # path('course/<slug:courseid>', views.courseDetails),
+]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
